4a2d32dd871c107bcfad2eb93494ef04171efcae
Handy-caT <37216852+Handy-caT@users.noreply.github.com>
2022-02-25 22:38:21 +0300

    min_max added

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "windows.h"
 
-
+std::pair<int,int> min_max(int* array, int size) {
+    int i = 0;
+    int max = array[0];
+    int min = max;
+    while(i < size) {
+        if(array[i] > max) max = array[i];
+        Sleep(7);
+        if(array[i] < min) min = array[i];
+        Sleep(7);
+        ++i;
+    }
+    return std::pair<int,int>(min,max);
+}
 
 int main() {
     int size;
