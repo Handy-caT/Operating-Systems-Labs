15c776d455440ab157d8e8fda2ae4682dbf250e6
Handy-caT <37216852+Handy-caT@users.noreply.github.com>
2022-02-25 23:11:30 +0300

    main added

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,21 +1,41 @@
 #include <iostream>
-#include "windows.h"
+#include <conio.h>
+#include <windows.h>
 
-std::pair<int,int> min_max(int* array, int size) {
+
+struct min_max_struct {
+    int* array;
+    int size;
+    std::pair<int,int>& pair;
+};
+
+struct average_struct {
+    int* array;
+    int size;
+    int& result;
+};
+
+DWORD WINAPI min_max(void* params) {
+    min_max_struct* a = (min_max_struct*)params;
+    int size = a->size;
+    int* array = a->array;
     int i = 0;
     int max = 0;
     int min = 0;
     while(i < size) {
-        if(array[i] > max) max = i;
+        if(array[i] > array[max]) max = i;
         Sleep(7);
-        if(array[i] < min) min = i;
+        if(array[i] < array[min]) min = i;
         Sleep(7);
         ++i;
     }
-    return std::pair<int,int>(min,max);
+    a->pair =  std::pair<int,int>(min,max);
 }
 
-int average(int* array, int size) {
+DWORD WINAPI average(void* params) {
+    average_struct* a = (average_struct*)params;
+    int size = a->size;
+    int* array = a->array;
     int i = 0;
     int average = 0;
     while (i < size) {
@@ -23,7 +43,7 @@ int average(int* array, int size) {
         Sleep(12);
         ++i;
     }
-    return average/size;
+    a->result = average/size;
 }
 
 int main() {
@@ -31,16 +51,31 @@ int main() {
     int* array;
 
     printf("Input array size:");
-    scanf("%i",size);
+    scanf("%i",&size);
     array = new int[size];
     printf("Input array numbers:");
 
     int i = 0;
     while(i < size) {
-        scanf("%i",array[i]);
+        scanf("%i",&array[i]);
         ++i;
     }
+    std::pair<int,int> pair;
+    min_max_struct a = {array,size,pair};
+    int result;
+    average_struct b = {array,size,result};
+    HANDLE averageThread = CreateThread(NULL,0,average,&b, 0, NULL);
+    HANDLE minMaxThread = CreateThread(NULL,0,min_max,&a, 0, NULL);
+    WaitForSingleObject(averageThread,INFINITE);
+    WaitForSingleObject(minMaxThread,INFINITE);
 
+    array[a.pair.first] = b.result;
+    array[a.pair.second] = b.result;
 
+    i = 0;
+    while(i < size) {
+        printf("%i ",array[i]);
+        ++i;
+    }
 
 }
