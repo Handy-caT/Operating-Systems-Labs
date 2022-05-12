b1e260298882ab3ecac9e7f907bc963dad0abb34
Handy-caT <37216852+Handy-caT@users.noreply.github.com>
2022-05-12 22:26:26 +0300

    Marker added

--- lab3/marker.h
+++ lab3/marker.h
@@ -1,14 +1,26 @@
 //
 // Created by Максим on 11.03.22.
 //
+#include "windows.h"
+#include <vector>
 
 #ifndef LAB3_MARKER_H
 #define LAB3_MARKER_H
 
 struct markerArguments {
     int* array;
+    int& markerToStop;
     int size;
-    int number;
+    int markerNumber;
+
+    CRITICAL_SECTION &criticalSection;
+    HANDLE startEvent;
+    std::vector<HANDLE> &threads;
+
+    markerArguments(int *array, int size, int number, const CRITICAL_SECTION &criticalSection, const void *startEvent,
+                    std::vector<HANDLE> &threads, int &markerToStop) : array(array), size(size), markerNumber(number),
+                                                                       criticalSection(const_cast<CRITICAL_SECTION &>(criticalSection)), startEvent(const_cast<HANDLE>(startEvent)),
+                                                                       threads(threads), markerToStop(markerToStop) {}
 };
 
 DWORD WINAPI marker(void* arguments);
